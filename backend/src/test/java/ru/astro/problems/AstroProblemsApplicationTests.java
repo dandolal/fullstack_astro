5dc6597712a2a2189512efa9c7cb048e930f642a
dandolal <dan.dolgov@gmail.com>
2020-12-18 05:16:35 +0300

    initial

---
+++ backend/src/test/java/ru/astro/problems/AstroProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.astro.problems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstroProblemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
